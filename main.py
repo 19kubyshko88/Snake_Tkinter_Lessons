ab62583be541a8391133d65926d513c9944dec47
19kubyshko88 <19kubyshko88@gmail.com>
2023-10-22 00:13:48 +0300

    feat: Движение головы вправо.

--- main.py
+++ main.py
@@ -31,9 +31,24 @@ class Snake(object):
     def __init__(self, segments):
         self.segments = segments
 
+    def move(self):
+        x1, y1, x2, y2 = c.coords(self.segments[0].instance)  # получаем координаты сегмента головы
+        c.coords(self.segments[-1].instance,  # при перемещении головы меняются координаты
+                 x1 + SEG_SIZE, y1,                   # левого верхнего и
+                 x2 + SEG_SIZE, y2)                      # правого нижнего угла прямоугольника.
 
-segments = [Segment(SEG_SIZE, SEG_SIZE), ] # создаем набор сегментов. пока одна голова
+
+segments = [Segment(SEG_SIZE, SEG_SIZE), ]  # создаем набор сегментов. пока одна голова
 
 s = Snake(segments)  # собственно змейка
+# s.move()  # Просто вызов не работает - картинка не обновляется. Нужен root.after.
+
+
+def main():
+    s.move()
+    root.after(100, main)
+
+
+main()
 
 root.mainloop()  # Запускаем окно
